ee72b291993c67cc63051ac41b26046a3fd8ebc0
iexus <2.-@twopointline.com>
2020-12-22 13:54:01 +0000

    Getting the filenames

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::process::Command;
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let output = Command::new("git")
+        .args(&["ls-tree", "-r", "--name-only", "HEAD"])
+        .output()
+        .expect("failed to execute git ls-tree - is git installed?");
+
+    println!("{}", output.status);
+
+    io::stdout().write_all(&output.stdout).unwrap();
 }
